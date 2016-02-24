a1508177c7726e74b15f1eb65117e59bf67103d3
Teague Sterling <teaguesterling@gmail.com>
2016-02-24 11:52:13 -0800

    Adding additional properties to drugs.ldjson

--- scripts/fetchdrugs.py
+++ scripts/fetchdrugs.py
@@ -1,19 +1,23 @@
 #!/nfs/soft/www/apps/zinc15/envs/production/bin/python
 # fetchdrugs.py
 
-import os,sys
-import logging
 from collections import namedtuple
-from zinc.data.models.core import undefer
+import itertools
+import logging
+import os
+import sys
+from zinc.data.models.core import joinedload
 from zinc.management import build_context
 from zinc.data.representations import object_formatter
 
 #code =sys.argv[1]
 
 build_context(context=globals(),environ=os.environ)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 fields = [
-   "inchikey","name","synonyms","atc_classifications", "genes", "trials", "zinc_id", "drug_class", "external_references", "image_url", 
+   "inchikey","name","synonyms","atc_classifications", "purchasable",
+   "genes", "activities", "trials", "zinc_id", 
+   "drug_class", "external_references", "image_url", 
 ]
 
 formatter = object_formatter('ldjson', fields)
@@ -24,21 +28,34 @@ q = db.session.query(Substance)
 #q = Substance.query.filter(Substance.atc_code_names.in_([code]))
 #q = q.with_subsets(['investigational'])  # This seems to be buggy (hidden subset)
 q = q.filter(Substance.features.overlap(['fda', 'world', 'investigational']))
-q = q.options(undefer(Substance.inchikey))
 q = q.yield_per(100)
 
 logging.info(str(q))
 
 def create_record(query):
     for result in query:
+        human_observations = result.observations\
+                .filter(Observation.ortholog_name.endswith('_HUMAN'))\
+                .options(joinedload(Observation.ortholog))
+        human_activities = sorted(
+            ((observation.ortholog.name, int(observation.affinity)) 
+                for observation in human_observations),
+            key=lambda item: (item[0], -item[1])
+        )
+        human_activities = itertools.groupby(human_activities, key=lambda item: item[0])
+        human_activities = [list(group)[0] for key, group in human_activities]
+        human_activities = {ortholog: affinity for ortholog, affinity in human_activities}
+
         data = {}
         data['inchikey'] = result.inchikey
         data['name'] = result.preferred_name
         data['synonyms'] = result.synonym_names
+        data['purchasable'] = result.purchasable >= 10
         data['atc_classifications'] = [atc.__json__() for atc in result.atc_classifications]
         data['genes'] = result.gene_names
         data['trials'] = [trial.code for trial in result.trials.with_subsets(['cancer'])]
         data['zinc_id'] = result.zinc_id
+        data['activities'] = human_activities
         data['drug_class'] = ''
         features=result.features or ()
         if 'fda' in features:
